9ff2eee286952ff7e27cea714bae185ca18c917b
trung <trung.duc.nguyenhp@gmail.com>
2023-05-26 21:24:55 +0700

    mlflow-day2

--- week2/train.py
+++ week2/train.py
@@ -1,10 +1,13 @@
 import os
 import pickle
 import click
+import mlflow
 
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.metrics import mean_squared_error
 
+mlflow.set_tracking_uri("sqlite:///mlflow.db")
+mlflow.set_experiment("nyc-taxi-experiment")
 
 def load_pickle(filename: str):
     with open(filename, "rb") as f_in:
@@ -17,17 +20,31 @@ def load_pickle(filename: str):
     default="./output",
     help="Location where the processed NYC taxi trip data was saved"
 )
-def run_train(data_path: str):
+def run_train(data_path: str) :
 
-    X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))
-    X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+    with mlflow.start_run():
 
-    rf = RandomForestRegressor(max_depth=10, random_state=0)
-    rf.fit(X_train, y_train)
-    y_pred = rf.predict(X_val)
+        #first param is the key, second is the value
+        mlflow.set_tag("learner", "trung")
 
-    rmse = mean_squared_error(y_val, y_pred, squared=False)
+        #log the data
+        mlflow.log_param("data_path", data_path)
+        mlflow.log_param("model_name", "random_forest")
 
+        X_train, y_train = load_pickle(os.path.join(data_path, "train.pkl"))        
+        X_val, y_val = load_pickle(os.path.join(data_path, "val.pkl"))
+            
+        rf = RandomForestRegressor(max_depth=10, random_state=0)
 
+        #log the hyperparameters
+        mlflow.log_param("max_depth", 10)
+
+        rf.fit(X_train, y_train)
+        y_pred = rf.predict(X_val)
+        rmse = mean_squared_error(y_val, y_pred, squared=False)
+    
+        mlflow.log_metric("rmse", float(rmse))
+        
+        
 if __name__ == '__main__':
     run_train()
