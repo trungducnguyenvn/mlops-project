70c8eeadf3a5095a7410c8803c8d82292e53b6df
trung <trung.duc.nguyenhp@gmail.com>
2023-06-12 14:42:39 +0700

    Finish week3-Orchestration

--- week3/orchestrate_artifact.py
+++ week3/orchestrate_artifact.py
@@ -9,6 +9,8 @@ from sklearn.metrics import mean_squared_error
 import mlflow
 import xgboost as xgb
 from prefect import flow, task
+from prefect.artifacts import create_markdown_artifact
+from datetime import date
 
 
 
@@ -107,13 +109,29 @@ def train_best_model(
         mlflow.log_artifact("models/preprocessor.b", artifact_path="preprocessor")
 
         mlflow.xgboost.log_model(booster, artifact_path="models_mlflow")
+
+        markdown__rmse_report = f"""# RMSE Report
+
+        ## Summary
+
+        Duration Prediction 
+
+        ## RMSE XGBoost Model
+
+        | Region    | RMSE |
+        |:----------|-------:|
+        | {date.today()} | {rmse:.2f} |
+        """
+
+        create_markdown_artifact(key="duration-model-report", markdown=markdown__rmse_report)
+        
     return None
 
 
 @flow
-def main_flow(
-    train_path: str = "./data/green_tripdata_2023-01.parquet",
-    val_path: str = "./data/green_tripdata_2023-02.parquet",
+def main_flow_artifact(
+    train_path: str = "./data/green_tripdata_2023-02.parquet",
+    val_path: str = "./data/green_tripdata_2023-03.parquet",
 ) -> None:
     """The main training pipeline"""
 
@@ -133,4 +151,4 @@ def main_flow(
 
 
 if __name__ == "__main__":
-    main_flow()
+    main_flow_artifact()
